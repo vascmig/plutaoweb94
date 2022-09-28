3f2485d64edd81dd24fc971892c05e8797a3622c
vascmig <miguel.vasconcelos@usp.br>
2022-09-28 23:14:08 +0000

    funcionalidade cadastro desenvolvida

---
+++ spec/models/aluno_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Aluno, type: :model do
+  it 'invalido sem nome' do
+    aluno = Aluno.new
+    aluno.email = "teste@gmail.com"
+    expect(aluno).not_to be_valid
+  end
+  it 'invalido sem email' do
+    aluno = Aluno.new
+    aluno.nome = "jose"
+    expect(aluno).not_to be_valid
+  end  
+  it 'invalido com email com formato invalido' do
+    aluno = Aluno.new
+    aluno.nome = "jose"
+    aluno.email = "jose,#!@!gmail.com"
+    expect(aluno).not_to be_valid
+  end  
+end
